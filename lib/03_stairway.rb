cc419fab4663a70441b624a071bfe242935a0d5c
tsiry <YOUR@EMAIL.com>
2019-07-12 11:14:55 +0300

    update exo 3

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,6 +1,6 @@
 puts "vous commencer à la position 0 "
 puts "--------cliquez sur entrer pour jouer----------"
-
+def game
 #------->declaration des variables
 play = gets.chomp
 joueur = 0
@@ -12,8 +12,8 @@ joueur = 0
 random = rand(1..6)
 	if random === 5 || random === 6
 
-		 	puts "vous etes deplacer de #{random} position"
-		 	puts "vous etes maintenant dans la #{joueur} position"
+		 	puts "le dé c'est arrêter à #{random}"
+		 	puts "vous êtes maintenant dans la #{joueur+1} position"
 		 	puts "cliquez sur entrer"
 		joueur += 1 
 		play = gets.chomp
@@ -21,17 +21,16 @@ random = rand(1..6)
 
 #------>condition decrémentation
 	 elsif random === 1
-
-			puts "vous etes sur la positon #{joueur}, cliquez sur x pour continuer" 
-			puts "vous avez reculer de #{random} position"
+	 		puts "vous avez reculer de #{random} position"
+			puts "vous êtes sur la positon #{joueur-1}, car le dé c'est arrêter à #{random}" 
 			puts "cliquez sur entrer"
-		joueur -= random
+		joueur -= 1
 		play = gets.chomp
 
 #-------> condition reste en position initial
 	 else
-			puts "Vous etes rester sur votre position #{joueur}"
-			puts "votre Dé c'est arreter sur le chiffre #{random}"
+			puts "Vous êtes rester sur votre position #{joueur}"
+			puts "Car, votre Dé c'est arrêter sur le chiffre #{random}"
 			puts "cliquez sur entrer"
 		joueur = joueur
 		play = gets.chomp
@@ -43,4 +42,6 @@ puts"-------vous avez atteint la position final----------"
 puts"		------GAGNEZ----------"
 puts"		------GAGNEZ----------"
 puts"		------GAGNEZ----------"
-
+end
+puts "#{game}"
+ 
